a2cfd68231e8e0dfe980cd4b2439bb9070bd3c76
Tran-Mai-Anh <155942841+Tran-Mai-Anh@users.noreply.github.com>
2025-03-17 00:36:33 +0700

    upload category from admin to client

--- Cosmetic/Controllers/HomeController.cs
+++ Cosmetic/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
-using System.Diagnostics;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Cosmetic.Data;  
+using Shop.Models;  
 
 namespace Cosmetic.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CosmeticContext _context; 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CosmeticContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -17,28 +24,55 @@ namespace Cosmetic.Controllers
             return View();
         }
 
-        public IActionResult Category()
+        public async Task<IActionResult> Category(int? categoryId)
         {
-            return View();
+            if (categoryId.HasValue)
+            {
+                var categoryExists = await _context.Category.AnyAsync(c => c.ID == categoryId);
+                if (!categoryExists)
+                {
+                    return Content("Danh mục không hợp lệ!"); // Bạn có thể chuyển hướng về trang danh mục chính
+                }
+            }
+
+            var categories = await _context.Category
+                .Where(c => c.Status == "active")
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+
+            var products = await _context.Product
+                .Where(p => !categoryId.HasValue || p.CategoryID == categoryId)
+                .OrderByDescending(p => p.CreateTime)
+                .ToListAsync();
+
+            ViewBag.SelectedCategoryId = categoryId;
+            ViewBag.Categories = categories;
+
+            return View(products);
         }
+
+
+
 
         public IActionResult ProductDetail()
         {
             return View();
         }
+
         public IActionResult ShoppingCart()
         {
             return View();
         }
+
         public IActionResult CheckOut()
         {
             return View();
         }
+
         public IActionResult AboutUs()
         {
             return View();
         }
-
 
         public IActionResult Compare()
         {
