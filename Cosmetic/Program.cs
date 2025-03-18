a65aabeccc19ea3fe6453471091aad7d9a5e1578
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-03-18 13:47:46 +0700

    Update Program.cs

--- Cosmetic/Program.cs
+++ Cosmetic/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Cosmetic.Data;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
 //builder.Services.AddDbContext<CosmeticContext>(options =>
 //options.UseSqlServer(builder.Configuration.GetConnectionString("CosmeticContext") ?? throw new InvalidOperationException("Connection string 'CosmeticContext' not found.")));
@@ -20,8 +24,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
